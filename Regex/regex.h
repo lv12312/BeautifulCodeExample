783e3e5b0735caf7fe5fa0dde14755b7674b1218
lv12312 <lv-12312@163.com>
2013-12-05 23:49:48 +0800

    修改字符集为UTF-8

--- Regex/regex.h
+++ Regex/regex.h
@@ -1,12 +1,7 @@
-/*
- * regex.h
- *
- *  Created on: 2013Äê12ÔÂ5ÈÕ
- *      Author: Lemon
- */
-
 #ifndef REGEX_H_
 #define REGEX_H_
 
 int match(char *regexp, char *text);
+int matchstar(int c, char *regexp, char *text);
+int matchhere(char *regexp, char *text);
 #endif /* REGEX_H_ */
